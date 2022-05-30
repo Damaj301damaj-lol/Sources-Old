625af7ab080058af257a5eb4c6df2023e605947b
Mohamad Damaj <mohamad301damaj@duck.com>
2022-05-30 21:40:44 +0300

    updated

--- dwm/config.h
+++ dwm/config.h
@@ -5,20 +5,15 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "terminus:size=9" };
+static const char *fonts[]          = { "terminus:size=10" };
 static const char dmenufont[]       = "terminus:size=8";
 static const char col_gray1[]  = "#179dfd";
 static const char col_gray2[]  = "#1c1c1c";
 static const char col_gray3[]  = "#403f3f";
 static const char col_gray4[]  = "#666560";
-/*static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee"; */
 static const char col_dmenu1[]      = "#FD7717";
 static const char col_dmenu2[]      = "#ffffff";
 static const char col_dmenu3[]      = "#000000";
-/*static const char col_cyan[]        = "#FD7717"; */
 static const char col_cyan[]        = "#179dfd";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
